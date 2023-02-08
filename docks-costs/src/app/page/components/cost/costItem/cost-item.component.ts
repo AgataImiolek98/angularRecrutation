28c559f51c900459d479c2345cc5efab62647561
AGATA_IMIOLEK <imiolekagata@yahoo.com>
2023-02-08 13:31:42 +0100

    unsubscribe Observable

--- docks-costs/src/app/page/components/cost/costItem/cost-item.component.ts
+++ docks-costs/src/app/page/components/cost/costItem/cost-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { FormControl } from "@angular/forms";
 import { faComment } from '@fortawesome/free-solid-svg-icons';
-import { BehaviorSubject, combineLatest } from "rxjs";
+import { BehaviorSubject, combineLatest, Subject, takeUntil } from "rxjs";
 import { OnlyNumberDirective } from "src/app/page/directives/only-number.directive";
 import { Categories, CostItem, CostItemDetails } from "src/app/page/models/costs.config.interface";
 import { PaymentCurrency } from "src/app/page/models/exchange-rates.config.interface";
@@ -22,6 +22,7 @@ export class CostItemComponent implements OnInit {
     showMe: boolean = false;
     @Input() costItem: CostItem;
     categories$: BehaviorSubject<Categories> = this.categoriesService.categories$;
+    unsubscribe$: Subject<void> = new Subject<void>
 
     screenedCost: CostItemDetails;
     quotedCost: CostItemDetails;
@@ -44,16 +45,23 @@ export class CostItemComponent implements OnInit {
         combineLatest([
             this.currencyAmountService.selectedCurrencyExchangeRate$,
             this.screenedCostControl.valueChanges
-        ]).pipe()
-        .subscribe(([selectedCurrencyExchangeRate, value]) => {
+        ]).pipe(
+        takeUntil(this.unsubscribe$)
+        ).subscribe(([selectedCurrencyExchangeRate, value]) => {
             this.quotedAmountInUSD = this.currencyAmountService.getValueInUsd(this.quotedCost.amount, selectedCurrencyExchangeRate)
             this.screenedAmountInUSD = this.currencyAmountService.getValueInUsd(value, selectedCurrencyExchangeRate)
         })
         
     }
 
+    ngOnDestroy(): void {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
+    }
+
     showComment() {
         this.showMe=!this.showMe;
       }
+
 
 }
